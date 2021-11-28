34c45d9894d9e1d860394088491314c3c27bc016
Skill <dima3000mega@gmail.com>
2021-11-28 15:52:27 +0200

    add random range

--- random/src/random.rs
+++ random/src/random.rs
@@ -1,10 +1,10 @@
 use std::mem;
+use rand::prelude::SliceRandom;
 use rand::{Rng, thread_rng};
 use serde_json::Value;
 use uuid::Uuid;
 
 use crate::utils::cstring;
-use crate::utils::random::shuffle_array;
 use crate::{ERR, string_to_ptr, unwrap_or_err};
 
 #[no_mangle]
@@ -20,7 +20,8 @@ pub extern "stdcall" fn shuffle(array_ptr: *const u16) -> *const u16 {
             return string_to_ptr!(ERR);
         }
     };
-    shuffle_array(array);
+    let mut rng = thread_rng();
+    array.shuffle(&mut rng);
 
     string_to_ptr!(&unwrap_or_err!(serde_json::to_string(array)))
 }
@@ -42,6 +43,26 @@ pub extern "stdcall" fn choice(array_ptr: *const u16) -> *const u16 {
     let mut rng  = thread_rng();
 
     string_to_ptr!(array[rng.gen_range(0..array.len())].as_str().unwrap_or_default())
+}
+
+#[no_mangle]
+pub extern "stdcall" fn range(from_ptr: *const u16, to_ptr: *const u16) -> *const u16 {
+    let from = unwrap_or_err!(cstring::from_ptr(from_ptr)
+                        .unwrap()
+                        .parse::<i64>());
+
+    let to = unwrap_or_err!(cstring::from_ptr(to_ptr)
+                        .unwrap()
+                        .parse::<i64>());
+
+    if from > to {
+        let mut err = String::from(ERR);
+        err.push_str("`from` can't be larger than `to`");
+        return string_to_ptr!(&err);
+    }
+
+    let mut rng  = thread_rng();
+    string_to_ptr!(&rng.gen_range(from..to).to_string())
 }
 
 #[no_mangle]
--- random/src/utils/random.rs
+++
@@ -1,8 +0,0 @@
-use serde_json::Value;
-
-use rand::{prelude::SliceRandom, thread_rng};
-
-pub fn shuffle_array(array: &mut Vec<Value>) {
-    let mut rng = thread_rng();
-    array.shuffle(&mut rng);
-}
