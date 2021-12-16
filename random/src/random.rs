9da067176b927072d4ece4d8782b53f2d29ae10b
Skill <dima3000mega@gmail.com>
2021-12-16 17:00:06 +0200

    range and rangef

--- random/src/random.rs
+++ random/src/random.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::mem;
 use rand::prelude::SliceRandom;
 use rand::{Rng, thread_rng};
@@ -9,7 +10,7 @@ use crate::{ERR, string_to_ptr, unwrap_or_err};
 
 #[no_mangle]
 pub extern "stdcall" fn shuffle(array_ptr: *const u16) -> *const u16 {
-    let array = cstring::from_ptr(array_ptr).unwrap();
+    let array = cstring::from_ptr(array_ptr);
     let mut v: Value = unwrap_or_err!(serde_json::from_str(&array));
 
     let array = match v.as_array_mut() {
@@ -28,7 +29,7 @@ pub extern "stdcall" fn shuffle(array_ptr: *const u16) -> *const u16 {
 
 #[no_mangle]
 pub extern "stdcall" fn choice(array_ptr: *const u16) -> *const u16 {
-    let array = cstring::from_ptr(array_ptr).unwrap();
+    let array = cstring::from_ptr(array_ptr);
     let v: Value = unwrap_or_err!(serde_json::from_str(&array));
 
     let array = match v.as_array() {
@@ -48,21 +49,52 @@ pub extern "stdcall" fn choice(array_ptr: *const u16) -> *const u16 {
 #[no_mangle]
 pub extern "stdcall" fn range(from_ptr: *const u16, to_ptr: *const u16) -> *const u16 {
     let from = unwrap_or_err!(cstring::from_ptr(from_ptr)
-                        .unwrap()
                         .parse::<i64>());
 
     let to = unwrap_or_err!(cstring::from_ptr(to_ptr)
-                        .unwrap()
                         .parse::<i64>());
 
-    if from > to {
-        let mut err = String::from(ERR);
-        err.push_str("`from` can't be larger than `to`");
-        return string_to_ptr!(&err);
+    match from.cmp(&to) {
+        Ordering::Less => {
+            let mut err = String::from(ERR);
+            err.push_str("`from` can't be larger than `to`");
+            return string_to_ptr!(&err);
+        }
+
+        Ordering::Equal => {
+            let mut err = String::from(ERR);
+            err.push_str("`from` cant't be the same as `to`");
+            return string_to_ptr!(&err);
+        }
+
+        _ => {}
     }
 
     let mut rng  = thread_rng();
     string_to_ptr!(&rng.gen_range(from..to).to_string())
+}
+
+#[no_mangle]
+pub extern "stdcall" fn rangef(from_ptr: *const u16, to_ptr: *const u16, precision_ptr: *const u16) -> *const u16 {
+    let from = unwrap_or_err!(cstring::from_ptr(from_ptr)
+                        .parse::<f64>());
+
+    let to = unwrap_or_err!(cstring::from_ptr(to_ptr)
+                        .parse::<f64>());
+
+    let precision = unwrap_or_err!(cstring::from_ptr(precision_ptr)
+                        .parse::<usize>());
+
+    if from > to || from == to {
+        let mut err = String::from(ERR);
+        err.push_str("`from` must be larger than `to`");
+        return string_to_ptr!(&err);
+    }
+
+    let mut rng  = thread_rng();
+    let rnd = rng.gen_range(from..to);
+
+    string_to_ptr!(&format!("{:.1$}", rnd, precision))
 }
 
 #[no_mangle]
@@ -74,14 +106,13 @@ pub extern "stdcall" fn uuidv4() -> *const u16 {
 
 #[no_mangle]
 pub extern "stdcall" fn fill_with(string_ptr: *const u16, fill_string_ptr: *const u16, num_ptr: *const u16, index_ptr: *const u16) -> *const u16 {
-    let mut string = cstring::from_ptr(string_ptr).unwrap();
-    let fill_string = cstring::from_ptr(fill_string_ptr).unwrap();
+    let mut string = cstring::from_ptr(string_ptr);
+    let fill_string = cstring::from_ptr(fill_string_ptr);
 
     let num = unwrap_or_err!(cstring::from_ptr(num_ptr)
-                        .unwrap()
                         .parse::<usize>());
 
-    let index = cstring::from_ptr(index_ptr).unwrap().parse::<usize>().unwrap_or_default();
+    let index = cstring::from_ptr(index_ptr).parse::<usize>().unwrap_or_default();
     if string.len() > num || index > string.len() {
         return string_to_ptr!(&string);
     }
--- random/src/utils/cstring.rs
+++ random/src/utils/cstring.rs
@@ -8,11 +8,11 @@ pub fn to_widechar<S: AsRef<OsStr> + ?Sized>(s: &S)-> Vec<u16> {
     OsStr::new(s).encode_wide().chain(once(0)).collect()
 }
 
-pub fn from_ptr(data_ptr: *const u16) -> Result<String, OsString> {
+pub fn from_ptr(data_ptr: *const u16) -> String {
     unsafe {
         let len = (0..).take_while(|&i| *data_ptr.offset(i)!=0).count();
         let slice = std::slice::from_raw_parts(data_ptr, len);
-        return OsString::from_wide(slice).into_string();
+        return OsString::from_wide(slice).into_string().unwrap();
     }
 }
 
